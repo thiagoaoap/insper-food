52ffd0b53b78bf790273e49aa0f3a065f1983fd6
Thiago <thiagoaoap@al.insper.edu.br>
2024-10-23 17:01:26 -0300

    login e senha (temporario)

--- app.py
+++ app.py
@@ -1,4 +1,26 @@
-class Pedido:
-    def __init__(self):
-        self.item()
-        self.senha()
+from flask import Flask, request
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+import getpass
+from pymongo.errors import OperationFailure
+
+user = input('User: ')
+password = getpass.getpass('Password: ')
+
+app = Flask(__name__)
+
+try:
+    app.config["MONGO_URI"] = f"mongodb+srv://{user}:{password}@cluster0.rvm0f.mongodb.net/insper_food"
+    mongo = PyMongo(app)
+    mongo.cx.server_info()
+    print("Conectado ao MongoDB com sucesso!")
+except OperationFailure as x:
+    print("Erro de autenticação: usuário ou senha incorretos.")
+    exit()
+except Exception as x:
+    print(f"Erro ao conectar ao MongoDB: {x}")
+    exit()
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False)
